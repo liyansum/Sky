6378f781cc4a19c46d6d8389f22e1acf29770d62
liyansum <103241475+liyansum@users.noreply.github.com>
2022-05-22 08:08:20 -0700

    Delete announcement_request.js

--- announcement_request.js
+++
@@ -1,9 +0,0 @@
- var headers = {
-'Accept-Encoding' : `gzip, deflate, br`,
-'Accept' : `*/*`,
-'Connection' : `keep-alive`,
-'Host' : `ma75.update.netease.com`,
-'User-Agent' : `Sky-iOS-Gold/187758 CFNetwork/1329 Darwin/21.3.0`,
-'Accept-Language' : `zh-CN,zh-Hans;q=0.9`,
-}
-$done(headers);
