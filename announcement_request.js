0e4ab5ae382bdbfbe5634f506b495fa505e0ebb7
liyansum <103241475+liyansum@users.noreply.github.com>
2022-05-22 20:41:16 +0800

    Create announcement_request.js

---
+++ announcement_request.js
@@ -0,0 +1,17 @@
+const url = `https://ma75.update.netease.com/game_notice/announcement_live.json`;
+const method = `GET`;
+const headers = {
+'Accept-Encoding' : `gzip, deflate, br`,
+'Accept' : `*/*`,
+'Connection' : `keep-alive`,
+'Host' : `ma75.update.netease.com`,
+'User-Agent' : `Sky-iOS-Gold/187758 CFNetwork/1329 Darwin/21.3.0`,
+'Accept-Language' : `zh-CN,zh-Hans;q=0.9`,
+};
+const body = ``;
+
+const myRequest = {
+    url: url,
+    method: method,
+    headers: headers,
+    body: body
