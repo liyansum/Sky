d3bba8c77eb87bc1e58964747136dfbcec93924b
liyansum <103241475+liyansum@users.noreply.github.com>
2022-05-22 21:19:13 +0800

    Update announcement_request.js

--- announcement_request.js
+++ announcement_request.js
@@ -1,9 +1,3 @@
-let headers = {
-'Accept-Encoding' : `gzip, deflate, br`,
-'Accept' : `*/*`,
-'Connection' : `keep-alive`,
-'Host' : `ma75.update.netease.com`,
-'User-Agent' : `Sky-iOS-Gold/187758 CFNetwork/1329 Darwin/21.3.0`,
-'Accept-Language' : `zh-CN,zh-Hans;q=0.9`,
-};
-$done(headers);
+let If-None-Match = ''
+let If-Modified-Since = ''
+$done();
